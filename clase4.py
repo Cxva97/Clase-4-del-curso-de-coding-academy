a03a3b4f5ab9ad870ccce12ab04a6d77ff7a933e
Cxva97 <cxva97@hotmail.com>
2025-11-07 13:26:19 -0500

    guardar cambios

---
+++ clase4.py
@@ -0,0 +1,15 @@
+#feedback condicionales taller variables estructuradas
+#actividad bucles
+#taller bucles try except
+
+"""
+feedback
+condicionales que unicamente es permitir que mi codigo tome decisiones
+segun las condiciones que yo le ponga
+
+1. if condicion :
+    #codigo o bloque a ejecutar
+2. if condicion :
+    #codigo o bloque a ejecutar
+   else:
+"""

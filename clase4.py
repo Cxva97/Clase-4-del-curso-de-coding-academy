07d2e8e4409d7dd2ab9586328b45f7c79616312f
Cxva97 <cxva97@hotmail.com>
2025-11-07 13:32:44 -0500

    agregando ejercicio

--- clase4.py
+++ clase4.py
@@ -12,4 +12,28 @@ segun las condiciones que yo le ponga
 2. if condicion :
     #codigo o bloque a ejecutar
    else:
+3. if condicion :
+    #codigo o bloque a ejecutar
+   elif condicion2:
+    #codigo o bloque a ejecutar
+   else:
+    #codigo o bloque a ejecutar
+
+4. anidaciones
+    if condicion:
+        if condicion2:  
+            #codigo o bloque a ejecutar
+        else:
+            #codigo o bloque a ejecutar
 """
+# si es mayor de edad y tiene mas de 500 de salario paga impuestos
+#si es menor no paga impuestos
+
+edad=int(input("ingrese su edad: "))
+salario=int(input("ingrese su salario: "))
+if edad >=18 and salario >500:
+    print("Paga impuestos del 20%")
+elif edad >=18 and salario <=500 :
+    print("paga impuestos del 0.15%")
+else:
+    print("no paga impuestos")
